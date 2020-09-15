cf6d29463024b40535017bb8595f3041b7145a6a
Kim Fitter <kimfitter@yahoo.com>
2020-09-15 15:59:12 +1200

    Adds types any types

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
+import {AnswerObject} from '../App'
+
+// Styles
+
 type Props = {
     question: string;
     answers: string[];
-    callback: any;
-    userAnswer: any;
+    callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+    userAnswer: AnswerObject | undefined;
     questionNr: number;
     totalQuestions: number;
 }
@@ -16,7 +20,7 @@ const QuestionCard: React.FC<Props> = ({ question, answers, callback, userAnswer
         <div>
             {answers.map(answer => (
                 <div key ={answer}>
-                    <button disabled={userAnswer} value = {answer} onClick={callback}>
+                    <button disabled={userAnswer ? true : false} value = {answer} onClick={callback}>
                         <span dangerouslySetInnerHTML={{ __html: answer }} />
                     </button>
                 </div>
